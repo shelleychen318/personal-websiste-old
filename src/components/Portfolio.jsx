b91c0f79498b91a9a66de63d82b6f1a500b46feb
Shelley Chen <shelleychen318@gmail.com>
2023-01-26 12:59:34 -0500

    fixed portfolio links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,49 +7,52 @@ const Portfolio = () => {
   const projects = [
     {
       id: 1,
-      src: project1,
+      image: project1,
       name: "Blog Website",
       description:
         "A personal blog website where I share my thoughts on various topics. Built using React, GraphQL, Nextjs, and hosted on Vercel.",
-      href: "https://javascript-blog.vercel.app/",
+      github: "https://github.com/shelleychen318/javascript-blog",
+      demo: "https://javascript-blog.vercel.app/",
     },
     {
       id: 2,
-      src: random2,
-      name: "Personal Blog",
-      description: "A personal blog",
+      image: random2,
+      name: "Project Title",
+      description: "Project Description",
+      github: "https://github.com/shelleychen318/javascript-blog",
+      demo: "https://javascript-blog.vercel.app/",
     },
     {
       id: 3,
-      src: project1,
-      name: "Personal Blog",
-      description: "A personal blog",
+      image: project1,
+      name: "Project Title",
+      description: "Project Description",
+      github: "https://github.com/shelleychen318/javascript-blog",
+      demo: "https://javascript-blog.vercel.app/",
     },
     {
       id: 4,
-      src: project1,
-      name: "Personal Blog",
-      description:
-        "A personal blog website where I share my thoughts on various topics. Built using React, GraphQL, Nextjs, and hosted on Vercel.",
+      image: random2,
+      name: "Project Title",
+      description: "Project Description",
+      github: "https://github.com/shelleychen318/javascript-blog",
+      demo: "https://javascript-blog.vercel.app/",
     },
     {
       id: 5,
-      src: project1,
-      name: "Personal Blog",
-      description: "A personal blog",
+      image: project1,
+      name: "Project Title",
+      description: "Project Description",
+      github: "https://github.com/shelleychen318/javascript-blog",
+      demo: "https://javascript-blog.vercel.app/",
     },
     {
       id: 6,
-      src: project1,
-      name: "Personal Blog",
-      description: "A personal blog",
-    },
-  ];
-
-  const githubs = [
-    {
-      id: 1,
-      href: "https://github.com/shelleychen318/javascript-blog",
+      image: project1,
+      name: "Project Title",
+      description: "Project Description",
+      github: "https://github.com/shelleychen318/javascript-blog",
+      demo: "https://javascript-blog.vercel.app/",
     },
   ];
 
@@ -74,13 +77,13 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {projects.map(({ id, src, name, description, href }) => (
+          {projects.map(({ id, image, name, description, github, demo }) => (
             <div className="shadow-md border-4 border-dark_beige rounded-2xl h-full duration-200 hover:scale-105">
-              <a key={id} href={href} target="_blank" rel="noreferrer">
-                <img src={src} alt="project" className="rounded-t-xl" />
+              <a key={id} href={demo} target="_blank" rel="noreferrer">
+                <img src={image} alt="project" className="rounded-t-xl" />
               </a>
               <div className="flex flex-col px-4 py-4">
-                <a key={id} href={href} target="_blank" rel="noreferrer">
+                <a key={id} href={demo} target="_blank" rel="noreferrer">
                   <p className="text-2xl font-medium pb-4 hover:underline underline-offset-4 decoration-2 decoration-text_colour">
                     {name}
                   </p>
@@ -88,29 +91,24 @@ const Portfolio = () => {
                 <p className="leading-7 pb-4">{description}</p>
 
                 <div className="flex flex-row space-x-4">
-                  {githubs.map(({ id, href }) => (
                     <a
                       key={id}
-                      href={href}
+                      href={github}
                       className="text-white w-fit px-4 py-2 my-4 flex items-center rounded-md text-pink font-medium border-pink border-2 hover:bg-pink hover:text-light_beige"
                       target="_blank"
                       rel="noreferrer"
                     >
                       GitHub
                     </a>
-                  ))}
-
-                  {/* {projects.map(({id, href}) => ( */}
                   <a
                     key={id}
-                    href={href}
+                    href={demo}
                     className="text-white w-fit px-4 py-2 my-4 flex items-center rounded-md font-medium bg-pink border-pink border-2 text-light_beige hover:bg-light_beige hover:text-pink"
                     target="_blank"
                     rel="noreferrer"
                   >
                     Demo
                   </a>
-                  {/* // ))} */}
                 </div>
               </div>
             </div>
